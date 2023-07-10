0d6bf6dd27021e3d9b040da4d2c909f0a66f15f8
Jerry.jiao <nz3c05xk@duck.com>
2023-07-10 06:23:29 +0000

    feat: starter

---
+++ nuxt.config.ts
@@ -0,0 +1,16 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  modules: [
+    '@unocss/nuxt',
+  ],
+  unocss: {
+    uno: true, // enabled `@unocss/preset-uno`
+    icons: true, // enabled `@unocss/preset-icons`
+    attributify: true, // enabled `@unocss/preset-attributify`,
+    // core options
+    shortcuts: [],
+    rules: [],
+    safelist: [],
+  },
+})
---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <h1 class="bg-blue-200">
+    Index Page
+  </h1>
+</template>
